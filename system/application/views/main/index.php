19e7f9c9f8c8bd93903f44b8e9255b2b81c061f1
root <root@b7898cad-be7b-4f9d-a3ab-260163951d67>
2008-11-05 16:44:37 +0000

    updating with Google ads and analytics

--- system/application/views/main/index.php
+++ system/application/views/main/index.php
@@ -48,3 +48,12 @@ echo '</table>';
 </td>
 </tr>
 </table>
+<br/>
+<center>
+<script type="text/javascript"><!--
+google_ad_client = "pub-2135094760032194";
+/* 468x60, created 11/5/08 */
+google_ad_slot = "4582459016"; google_ad_width = 468; google_ad_height = 60; //-->
+</script>
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+</center>
--- system/application/views/template2.php
+++ system/application/views/template2.php
@@ -50,5 +50,14 @@
 </table>
 </center>
 
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-246789-3");
+pageTracker._trackPageview();
+</script>
+
 </body>
 </html>
