675c91e333ca7e7e759c679eb6a34e3029598700
enygma <enygma@b7898cad-be7b-4f9d-a3ab-260163951d67>
2009-01-05 02:58:28 +0000

    updating API docs

--- system/application/views/api/out_xml.php
+++ system/application/views/api/out_xml.php
@@ -8,10 +8,10 @@ $resp=$doc->createElement('response');
 $doc->appendChild($resp);
 
 foreach($data as $k=>$v){
-	foreach($v as $ik=>$iv){
-		$it=$doc->createElement('item');
-		$resp->appendChild($it);
+	$it=$doc->createElement('item');
+	$resp->appendChild($it);
 	
+	foreach($v as $ik=>$iv){	
 		$elem=$doc->createElement($ik,$iv);
 		$it->appendChild($elem);
 	}
