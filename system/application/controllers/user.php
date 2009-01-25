b27a6e517ea208ea3c2cb0f10b3655b400fb3199
enygma <enygma@b7898cad-be7b-4f9d-a3ab-260163951d67>
2009-01-25 17:10:38 +0000

    updating

--- system/application/controllers/user.php
+++ system/application/controllers/user.php
@@ -28,7 +28,8 @@ class User extends Controller {
 		$this->validation->set_fields($fields);
 		
 		if($this->validation->run()==FALSE){
-			$this->session->set_userdata('ref_url',$_SERVER['HTTP_REFERER']);
+			$ref=(isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : '';
+			$this->session->set_userdata('ref_url',$ref);
 			$this->template->write_view('content','user/login');
 			$this->template->render();
 		}else{
