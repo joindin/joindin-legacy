b4d995c465ac3a15053dc6ad8f33f9b6ba07f52f
enygma <enygma@b7898cad-be7b-4f9d-a3ab-260163951d67>
2009-01-25 17:29:20 +0000

    updating

--- system/application/views/event/detail.php
+++ system/application/views/event/detail.php
@@ -31,7 +31,7 @@ if($attend){
 }
 ?>
 <b>::></b> <a href="#" id="attend_link" onClick="markAttending(<?=$det->ID?>,<?=$showt?>);return false;"><?=$link_txt?></a>
-(<?php echo $attend; echo ($det->event_end<time()) ? 'attending so far':'said they attended'; ?>)
+(<?php echo $attend; echo (time()>=$det->event_end) ? ' attending so far':' said they attended'; ?>)
 </div>
 
 <?php if($admin){ ?>
