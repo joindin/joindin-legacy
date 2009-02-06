578bb56f99e6e31a353efb9eed62aa573cd59739
jsor <jsor@b7898cad-be7b-4f9d-a3ab-260163951d67>
2009-02-06 19:45:48 +0000

    - Fixing reqkey/seckey on homepages

--- system/application/controllers/main.php
+++ system/application/controllers/main.php
@@ -10,11 +10,16 @@ class Main extends Controller {
 		$this->load->helper('form');
 		$this->load->model('talks_model');
 		$this->load->model('event_model');
+		$this->load->helper('reqkey');
+		
+		$reqkey = buildReqKey();
 		
 		$arr=array(
 			'talks'	=> $this->talks_model->getPopularTalks(),
 			'events'=> $this->event_model->getUpcomingEvents(true),
-			'logged'=> $this->user_model->isAuth()
+			'logged'=> $this->user_model->isAuth(),
+			'reqkey' => $reqkey,
+			'seckey' => buildSecFile($reqkey)
 		);
 		
 		$this->template->write_view('content','main/index',$arr,TRUE);
