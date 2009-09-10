af5a1b16705dde00968d89c6a0c52fc301714c4c
root <root@b7898cad-be7b-4f9d-a3ab-260163951d67>
2009-09-10 16:47:46 +0000

    brining up to date with production

--- system/application/libraries/Twitter.php
+++ system/application/libraries/Twitter.php
@@ -13,10 +13,12 @@ class Twitter {
 		if(!$term){ return array(); } //echo 'term:'; print_r($term);
 		$ret=array();
 		$arr=(!is_array($term)) ? array($term) : $term;
+		if(!empty($arr)){
 		foreach($arr as $k=>$v){
 			$url=$this->api_url.str_replace('#','%23',$v); //echo $url;
 			$tmp=json_decode(file_get_contents($url));
 			foreach($tmp as $ok=>$ov){ $ret[]=$ov; }
+		}
 		}
 		echo '<!-- <pre>'; print_r($ret); echo '</pre> -->';
 		return $ret;
