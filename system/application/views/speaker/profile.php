1e02160f995e53f8a1f32ecdde1da9ac26e4d364
enygma <enygma@b7898cad-be7b-4f9d-a3ab-260163951d67>
2009-09-29 03:39:48 +0000

    updating

---
+++ system/application/views/speaker/profile.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Add or edit the speaker's profile
+ */
+menu_pagetitle('Manage Speaker Profile');
+
+$this->load->view('user/_nav_sidebar');
+
+?>
+
+<div class="menu">
+	<ul>
+	<li class="active"><a href="/speaker/profile">Speaker Profile</a>
+	<li><a href="/speaker/access">Profile Access</a>
+	</ul>
+	<div class="clear"></div>
+</div>
+<?php
+
+echo 'list of the speaker profiles';
+
+?>
+</br/><br/>
+<a href="/speaker/edit">create/edit profile</a>
