7f932236d61a739cca8aa9709adf34d70f5be363
enygma <enygma@b7898cad-be7b-4f9d-a3ab-260163951d67>
2009-10-10 18:54:33 +0000

    creating the "event related" tab on the event detail page

--- system/application/views/event/detail.php
+++ system/application/views/event/detail.php
@@ -228,7 +228,7 @@ $ct=0;
 	    <?php foreach($evt_sessions as $ik=>$iv): ?>
         	<tr class="<?php echo ($ct%2==0) ? 'row1' : 'row2'; ?>">
 		    <td>
-		    <?php $type = !empty($iv->tcid) ? $iv->tcid : 'Talk'; ?>
+		    <?php $type = !empty($iv->tcid) ? $iv->tcid : 'Talk'; $type='Social Event'; ?>
 		    <span class="talk-type talk-type-<?php echo strtolower(str_replace(' ', '-', $type)); ?>"
 			  title="<?php echo escape($type); ?>"><?php echo escape(strtoupper($type)); ?></span>
 		    </td>
