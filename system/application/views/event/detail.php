a979aaf1ba3f6de809c07c54142b7b74709095eb
enygma <enygma@b7898cad-be7b-4f9d-a3ab-260163951d67>
2009-10-21 15:53:47 +0000

    adding new Slides tab

--- system/application/views/event/detail.php
+++ system/application/views/event/detail.php
@@ -262,7 +262,7 @@ $ct=0;
 	<div id="estats">
 	    <h3>Event Statistics</h3>
 	    <table cellpadding="0" cellspacing="0" border="0">
-	    <tr><td><b>Number of Sessions:</b></td><td style="padding:3px""><?php echo count($talks); ?></td></tr>
+	    <tr><td><b>Number of Sessions:</b></td><td style="padding:3px"><?php echo count($talks); ?></td></tr>
 	    <tr><td><b>Last Comment:</b></td><td style="padding:3px"><?php echo date('m.d.Y H:i:s',$latest_comment[0]->max_date); ?></td></tr>
 	    <tr><td><b>Total # of Comments</b></td><td style="padding:3px""><?php echo $total_comment_ct; ?></td></tr>
 	    <tr><td><b>Average Session Rating</b></td><td style="padding:3px"><?php echo round($session_rate/count($talks),2); ?></td></tr>
