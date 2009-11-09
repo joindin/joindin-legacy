151ad5ab4187683ebb2d48fa93a37584e58a2c30
Chris Cornutt <enygma@phpdeveloper.org>
2009-11-09 06:51:58 -0600

    updating default database structure

--- curr_db.sql
+++ curr_db.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: confs
 -- ------------------------------------------------------
--- Server version	5.0.67-0ubuntu6
+-- Server version	5.0.45
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,22 +20,17 @@
 --
 
 DROP TABLE IF EXISTS `blog_cats`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `blog_cats` (
   `name` varchar(100) default NULL,
   `ID` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `blog_comments`
 --
 
 DROP TABLE IF EXISTS `blog_comments`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `blog_comments` (
   `title` varchar(100) default NULL,
   `author_id` int(11) default NULL,
@@ -44,31 +39,25 @@ CREATE TABLE `blog_comments` (
   `blog_post_id` int(11) default NULL,
   `author_name` varchar(100) default NULL,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=626 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=629 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `blog_post_cat`
 --
 
 DROP TABLE IF EXISTS `blog_post_cat`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `blog_post_cat` (
   `post_id` int(11) default NULL,
   `cat_id` int(11) default NULL,
   `ID` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `blog_posts`
 --
 
 DROP TABLE IF EXISTS `blog_posts`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `blog_posts` (
   `title` varchar(250) default NULL,
   `content` text,
@@ -78,30 +67,24 @@ CREATE TABLE `blog_posts` (
   `views` int(11) default NULL,
   PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `categories`
 --
 
 DROP TABLE IF EXISTS `categories`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `categories` (
   `cat_title` varchar(200) default NULL,
   `cat_desc` text,
   `ID` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `event_comments`
 --
 
 DROP TABLE IF EXISTS `event_comments`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `event_comments` (
   `event_id` int(11) default NULL,
   `comment` text,
@@ -112,15 +95,12 @@ CREATE TABLE `event_comments` (
   `cname` varchar(100) default NULL,
   PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=235 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `events`
 --
 
 DROP TABLE IF EXISTS `events`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `events` (
   `event_name` varchar(200) default NULL,
   `event_start` int(11) default NULL,
@@ -139,32 +119,27 @@ CREATE TABLE `events` (
   `event_href` varchar(600) default NULL,
   `event_cfp_start` int(11) default NULL,
   `event_cfp_end` int(11) default NULL,
+  `event_voting` varchar(1) default NULL,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=86 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=92 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `lang`
 --
 
 DROP TABLE IF EXISTS `lang`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `lang` (
   `lang_name` varchar(200) default NULL,
   `lang_abbr` varchar(20) default NULL,
   `ID` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `speaker_profile`
 --
 
 DROP TABLE IF EXISTS `speaker_profile`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `speaker_profile` (
   `user_id` int(11) default NULL,
   `country_id` int(11) default NULL,
@@ -182,31 +157,25 @@ CREATE TABLE `speaker_profile` (
   `picture` varchar(200) default NULL,
   `ID` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `talk_cat`
 --
 
 DROP TABLE IF EXISTS `talk_cat`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `talk_cat` (
   `talk_id` int(11) default NULL,
   `cat_id` int(11) default NULL,
   `ID` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=751 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=823 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `talk_comments`
 --
 
 DROP TABLE IF EXISTS `talk_comments`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `talk_comments` (
   `talk_id` int(11) default NULL,
   `rating` int(11) default NULL,
@@ -216,17 +185,15 @@ CREATE TABLE `talk_comments` (
   `private` int(11) default NULL,
   `active` int(11) default NULL,
   `user_id` int(11) default NULL,
+  `comment_type` varchar(10) default NULL,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=1707 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=1940 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `talks`
 --
 
 DROP TABLE IF EXISTS `talks`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `talks` (
   `talk_title` tinytext,
   `speaker` tinytext,
@@ -239,16 +206,13 @@ CREATE TABLE `talks` (
   `owner_id` int(11) default NULL,
   `lang` int(11) default NULL,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=986 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=999 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `tz`
 --
 
 DROP TABLE IF EXISTS `tz`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `tz` (
   `offset` int(11) default NULL,
   `cont` varchar(50) default NULL,
@@ -256,15 +220,12 @@ CREATE TABLE `tz` (
   `ID` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `user` (
   `username` varchar(100) default NULL,
   `password` varchar(32) default NULL,
@@ -275,16 +236,13 @@ CREATE TABLE `user` (
   `full_name` varchar(200) default NULL,
   `active` int(11) default NULL,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=836 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=915 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `user_admin`
 --
 
 DROP TABLE IF EXISTS `user_admin`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `user_admin` (
   `uid` int(11) default NULL,
   `rid` int(11) default NULL,
@@ -292,23 +250,19 @@ CREATE TABLE `user_admin` (
   `ID` int(11) NOT NULL auto_increment,
   `rcode` varchar(40) default NULL,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=586 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=613 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `user_attend`
 --
 
 DROP TABLE IF EXISTS `user_attend`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `user_attend` (
   `uid` int(11) default NULL,
   `eid` int(11) default NULL,
   `ID` int(11) NOT NULL auto_increment,
   PRIMARY KEY  (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=742 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=805 DEFAULT CHARSET=latin1;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -319,4 +273,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-10-18 14:16:33
+-- Dump completed on 2009-11-09 12:51:25
