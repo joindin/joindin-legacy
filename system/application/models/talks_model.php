cc177b1565de1770d964d213362b1c14a99793e0
Lorna <lorna@rivendell.local>
2010-01-18 03:29:32 +0800

    fixing logic on allowed_comments flag for talks

--- system/application/models/talks_model.php
+++ system/application/models/talks_model.php
@@ -75,8 +75,7 @@ class Talks_model extends Model {
 				select
 					talks.*,
 					CASE 
-						WHEN (talks.date_given > '.mktime(0,0,0).') THEN 1
-						WHEN (date_add(talks.date_given, interval 3 month) < '.mktime(0,0,0).') THEN 1
+						WHEN ((talks.date_given > '.mktime(0,0,0).') and (date_add(talks.date_given, interval 3 month) < '.mktime(0,0,0).')) THEN 1
 						WHEN (events.event_voting = "Y") THEN 1
 						ELSE 0
 						END as allow_comments,
