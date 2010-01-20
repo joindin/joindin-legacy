63c4953fbef3b2510743cf4e4581f775585d754e
Chris Cornutt <enygma@phpdeveloper.org>
2010-01-19 21:40:43 -0600

    adding in a controller and example cron job

---
+++ scripts/upcoming.php
@@ -0,0 +1,5 @@
+<?php
+define('IS_CRON',true);
+$_GET['external/twitter_event_add']=null;
+include('../index.php');
+?>
---
+++ system/application/controllers/external.php
@@ -0,0 +1,26 @@
+<?php
+
+class External extends Controller {
+	
+	public function External(){
+		parent::Controller();
+	}
+	
+	public function twitter_event_add(){
+		if(!defined('IS_CRON')){ echo 'here'; }else{ return false; }
+		
+		//http://conf.localhost/event/hot
+		$this->load->library('twitter');
+		$this->load->model('event_model');
+		
+		$events=$this->event_model->getUpcomingEvents(null);
+		
+		$msg="Joind.in Update: There's ".count($events)." great events coming up soon! ";
+		$msg.="Check them out! http://joind.in/event/upcoming";
+		
+		//$resp=$this->twitter->sendMsg($msg);
+		//var_dump($resp);
+	}
+}
+
+?>
