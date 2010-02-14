725758857fd0527ea43e7f916cc7c4e044658918
Chris Cornutt <enygma@phpdeveloper.org>
2010-02-14 14:42:20 -0600

    Fixing bug with emails sent when no event admin exists

--- system/application/controllers/event.php
+++ system/application/controllers/event.php
@@ -827,7 +827,10 @@ class Event extends Controller {
 		
 		$this->load->model('event_model');
 		$this->load->library('sendemail');
+		$this->load->library('twitter');
 		$this->event_model->approvePendingEvent($eid);
+		
+		//print_r($this->event_model->getEventDetail($eid));
 		
 		// If we have admins for the event, send them an email to let them know
 		$admin_list	= $this->event_model->getEventAdmins($eid);
@@ -835,6 +838,14 @@ class Event extends Controller {
 			$evt_detail	= $this->event_model->getEventDetail($eid);
 			$this->sendemail->sendEventApproved($eid,$evt_detail,$admin_list);
 		}
+		
+		// @todo get this and twitter class working with short URL
+		/*echo '<pre>';
+		$link=$this->twitter->short_bitly('http://joind.in/event/view/'.$eid); 
+		echo '</pre>';*/
+		
+		// Send the new approved event to Twitter
+		//$this->twitter->sendMsg($msg);
 		
 		// Finally, redirect back to the event!
 		redirect('event/view/'.$eid); 
@@ -1088,6 +1099,9 @@ class Event extends Controller {
 			$user	= $this->user_model->getUser($is_auth);
 			// Grab the event admins
 			$admins	= $this->event_model->getEventAdmins($eid);
+			
+			//If there's no event admins, we send it to the site admins
+			if(empty($admins)){ $admins=$this->user_model->getSiteAdminEmail(); }
 			
 			// Push the emails over to the mailer class
 			$evt_name	= $arr['detail'][0]->event_name;
