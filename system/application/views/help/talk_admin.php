faa30719bbf5ccf862c9ac1ca346a16f759c765d
Chris Cornutt <enygma@phpdeveloper.org>
2010-03-03 12:59:37 -0600

    Updating some more of the help documentation for talks/events

---
+++ system/application/views/help/talk_admin.php
@@ -0,0 +1,23 @@
+<h2>Talk Administration</h2>
+
+<h3>Working with Your Talk</h3>
+<p>
+Whether you're an event admin or just a speaker who has had their talk claim approved, there are a few things you need 
+to know about editing the details of your talk. You can get to the session's management by visiting the sesssion's page 
+and clicking on the <b>Edit Talk</b> button just below the details.
+</p>
+
+<h3>What's on the Form</h3>
+<p>
+Most of the fields are pretty obvious in what they do, but here's a quick rundown on some things you might not know:
+<ul>
+<li><b>Date and Time of Session</b><br/>The date/time of the session cannot be outside of the time of the event. If you try to change it outside of that, 
+you'll get an error.
+<li><b>Session Type</b><br/>You can change the session type if it's not correct (<a href="/help/user_guide_talks">see here</a> for more on session
+types).
+<li><b>Session Langugae:</b><br/>You can update the language to the correct one for your talk
+<li><b>Slides Link</b><br/>This is where you can specify the link to your presentation. By default it's empty so nothing will
+show on the talk's detail page. If this value is filled in, though, the link will automatically populate on the talk's details 
+with the URL given.
+</ul>
+</p>
