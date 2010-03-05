f7bb60336e77fabe94ab4848c4bcfd87da0e6010
Chris Cornutt <enygma@phpdeveloper.org>
2010-03-04 22:01:49 -0600

    Unlinking tracks that have no sessions (removes issues with empty track lists)

--- system/application/views/event/detail.php
+++ system/application/views/event/detail.php
@@ -332,8 +332,11 @@ $ct=0;
 		<?php
 		foreach($tracks as $k=>$tr){
 			echo '<div style="padding:3px">';
-			echo '<a style="font-size:13px" href="/event/view/'.$det->ID.'/track/'.$tr->ID.'">'.$tr->track_name.'</a><br/>';
-			echo $tr->track_desc;
+			if($tr->used>0){
+				echo '<a style="font-size:13px;font-weight:bold" href="/event/view/'.$det->ID.'/track/'.$tr->ID.'">'.$tr->track_name.'</a><br/>';
+			}else{ echo '<span style="font-size:13px;font-weight:bold;">'.$tr->track_name.'</span><br/>'; }
+			echo $tr->track_desc.'<br/>';
+			echo $tr->used.' sessions';
 			echo '</div>';
 		}
 		?>
