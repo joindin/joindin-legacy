efeab2b278f784a876035275c985c1ede2cf4416
Chris Cornutt <enygma@phpdeveloper.org>
2010-03-11 13:18:40 -0600

    Cleaned up the speaker controller a bit and made the upload checking conditional instead of required for a successful add/edit

--- system/application/controllers/speaker.php
+++ system/application/controllers/speaker.php
@@ -12,21 +12,21 @@ class Speaker extends Controller {
      * Work with the speaker's profile(s)
      */
     public function profile(){
-	$this->load->model('speaker_profile_model','sp');
+		$this->load->model('speaker_profile_model','sp');
 	
-	$arr		= array();
-	$udata		= $this->user_model->getUser($this->session->userdata('ID'));
-	$arr['pdata']	= $this->sp->getProfile($udata[0]->ID);
+		$arr		= array();
+		$udata		= $this->user_model->getUser($this->session->userdata('ID'));
+		$arr['pdata']	= $this->sp->getProfile($udata[0]->ID);
 
-	$profile_pic=null;
-	if(!empty($arr['pdata'][0]->picture)){
-	    $p=$this->config->item('user_data').'/'.$arr['pdata'][0]->picture;
-	    if(is_file($p)){ $profile_pic='/inc/img/profile/'.$arr['pdata'][0]->picture; }
-	    $arr['profile_pic']=$profile_pic;
-	}
+		$profile_pic=null;
+		if(!empty($arr['pdata'][0]->picture)){
+		    $p=$this->config->item('user_data').'/'.$arr['pdata'][0]->picture;
+		    if(is_file($p)){ $profile_pic='/inc/img/profile/'.$arr['pdata'][0]->picture; }
+		    $arr['profile_pic']=$profile_pic;
+		}
 	
-	$this->template->write_view('content','speaker/profile',$arr);
-	$this->template->render();
+		$this->template->write_view('content','speaker/profile',$arr);
+		$this->template->render();
     }
     /**
      * Create/modify the information in a speaker's profile
@@ -45,23 +45,23 @@ class Speaker extends Controller {
 
 		$fields=array(
 		    'full_name'	=> 'Full Name',
-		    'email'	=> 'Email',
+		    'email'		=> 'Email',
 		    'website'	=> 'Website',
-		    'blog'	=> 'Blog',
-		    'phone'	=> 'Phone',
+		    'blog'		=> 'Blog',
+		    'phone'		=> 'Phone',
 		    'job_title'	=> 'Job Title',
-		    'bio'	=> 'Bio',
+		    'bio'		=> 'Bio',
 		    'street'	=> 'Street',
-		    'city'	=> 'City',
-		    'zip'	=> 'Zip',
+		    'city'		=> 'City',
+		    'zip'		=> 'Zip',
 		    'country'	=> 'Country',
 		    'resume'	=> 'Resume',
 		    'picture'	=> 'Picture'
 		);
 		$rules=array(
 		    'full_name'	=> 'required',
-		    'email'	=> 'required|valid_email',
-		    'bio'	=> 'required'
+		    'email'		=> 'required|valid_email',
+		    'bio'		=> 'required'
 		);
 		$this->validation->set_rules($rules);
 		$this->validation->set_fields($fields);
@@ -76,66 +76,57 @@ class Speaker extends Controller {
 		if($this->validation->run()!=FALSE){
 		    // Set up the upload for the resume
 		    $config=array(
-			'upload_path'	=> $this->config->item('user_data'),
-			'allowed_types'	=> 'jpg|gif|png',
-			'overwrite'	=> true,
-			'max_size'	=> 2000,
-			'max_height'	=> 200,
-			'max_width'	=> 200
+				'upload_path'	=> $this->config->item('user_data'),
+				'allowed_types'	=> 'jpg|gif|png',
+				'overwrite'	=> true,
+				'max_size'	=> 2000,
+				'max_height'	=> 200,
+				'max_width'	=> 200
 		    );
 		    $this->load->library('upload',$config);
 
-		    /*$this->load->library('upload',array(
-			'resume'=>array(
-			    'upload_path'	=> $this->config->item('user_resume_path'),
-			    'allowed_types'=>'txt|doc|pdf','max_size'=>2000
-			),
-			'picture'=>array(
-			    'upload_path'	=> $this->config->item('user_pic_path'),
-			    'allowed_types'	=> 'gif|jpg|png',
-			    'max_size'=>2000,'max_height'=>100,'max_width'=>100
-			)
-		    ));*/
-
-		    // Let's go! Make our array to insert!
-
 		    // Check for picture upload...reset our filename if it's there
 		    if(isset($_FILES['picture']) && $_FILES['picture']['error']==0){
-			$ext=strrchr($_FILES['picture']['name'],'.');
-			$_FILES['picture']['name']='user_pic_'.$udata[0]->ID.$ext;
+				$ext=strrchr($_FILES['picture']['name'],'.');
+				$_FILES['picture']['name']='user_pic_'.$udata[0]->ID.$ext;
 		    }
 		    // Check for resume upload...reset out filename if it's there
 		    if(isset($_FILES['resume']) && $_FILES['resume']['error']==0){
-			$ext=strrchr($_FILES['resume']['name'],'.');
-			$_FILES['resume']['name']='user_resume_'.$udata[0]->ID.$ext;
+				$ext=strrchr($_FILES['resume']['name'],'.');
+				$_FILES['resume']['name']='user_resume_'.$udata[0]->ID.$ext;
 		    }
+		
+			// Only run the upload if they've given us an image
+			$up_data=array();
+			$up_err	='';
+			if(isset($_FILES['picture']['name']) && $_FILES['picture']['name']!=''){
 	    
-		    //$this->upload->do_upload('resume');
-		    $this->upload->do_upload('picture');
+		    	//$this->upload->do_upload('resume');
+		    	$this->upload->do_upload('picture');
 
-		    //$this->upload->do_upload('resume');
-		    $up_err = $this->upload->display_errors('', '');
-		    $up_data= $this->upload->data();
+		    	//$this->upload->do_upload('resume');
+		    	$up_err = $this->upload->display_errors('', '');
+		    	$up_data= $this->upload->data();
+			}
 
 		    $data=array(
-			'user_id'	=>$udata[0]->ID,
-			'country_id'	=>$this->input->post(),
-			'full_name'	=>$this->input->post('full_name'),
-			'contact_email'	=>$this->input->post('email'),
-			'website'	=>$this->input->post('website'),
-			'blog'		=>$this->input->post('blog'),
-			'phone'		=>$this->input->post('phone'),
-			'city'		=>$this->input->post('city'),
-			'zip'		=>$this->input->post('zip'),
-			'street'	=>$this->input->post('street'),
-			'job_title'	=>$this->input->post('job_title'),
-			'bio'		=>$this->input->post('bio'),
-			//'resume'	=>$rdata['file_name'],
+				'user_id'		=>$udata[0]->ID,
+				'country_id'	=>$this->input->post('country_id'),
+				'full_name'		=>$this->input->post('full_name'),
+				'contact_email'	=>$this->input->post('email'),
+				'website'		=>$this->input->post('website'),
+				'blog'			=>$this->input->post('blog'),
+				'phone'			=>$this->input->post('phone'),
+				'city'			=>$this->input->post('city'),
+				'zip'			=>$this->input->post('zip'),
+				'street'		=>$this->input->post('street'),
+				'job_title'		=>$this->input->post('job_title'),
+				'bio'			=>$this->input->post('bio')
 		    );
-		    if($up_data['file_name']){
-			$data['picture']=$up_data['file_name'];
+		
+		    if(isset($up_data['file_name'])){
+				$data['picture']=$up_data['file_name'];
 		    }
-		    //echo '<pre>'; print_r($data); echo '</pre>';
 
             if ($up_err) {
                 $this->validate->error_string=$up_err;
@@ -150,9 +141,9 @@ class Speaker extends Controller {
 		}else{
 		    // If there's not an data set, get from their profile
 		    if(empty($this->validation->email)){
-			$udata=$this->user_model->getUser($this->session->userdata('ID'));
-			$this->validation->email    = $udata[0]->email;
-			$this->validation->full_name= $udata[0]->full_name;
+				$udata=$this->user_model->getUser($this->session->userdata('ID'));
+				$this->validation->email    = $udata[0]->email;
+				$this->validation->full_name= $udata[0]->full_name;
 		    }
 		}
 
@@ -172,9 +163,9 @@ class Speaker extends Controller {
         }
 
 		$arr=array(
-		    'msg'	    => $msg,
+		    'msg'	    	=> $msg,
 		    'profile_pic'   => $profile_pic,
-            'countries' => $countries
+            'countries' 	=> $countries
 		);
 
 		$this->template->write_view('content','speaker/edit',$arr);
