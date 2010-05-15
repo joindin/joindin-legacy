23142a47de259d114a3883cc221d3fd25d628b99
Chris Cornutt <enygma@phpdeveloper.org>
2010-05-14 22:26:06 -0500

    adding view for "also given"

---
+++ src/system/application/views/talk/_also_given.php
@@ -0,0 +1,18 @@
+<div class="box">
+	<h4><?php echo $title; ?></h4>
+	<div class="ctn">
+	<?php $ct=0; ?>
+
+	<table cellpadding="10" cellspacing="0" border="0">
+	<?php 
+	foreach($talks as $t){
+	?>
+	<tr>
+	<td valign="top" width="50%" style="padding-bottom:10px">
+		<a href=""><?php echo $t->event_name; ?></a><br/>
+	</td>
+	</tr>
+	<?php } ?>
+	</table>
+	</div>
+</div>
