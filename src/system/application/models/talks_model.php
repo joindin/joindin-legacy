3df850e3a249b3bc7bb11993ff0ebd607720487f
Chris Cornutt <enygma@phpdeveloper.org>
2010-05-18 10:57:51 -0500

    adding additional check to remove a notice in the getTalks when there isntt datetime information

--- src/system/application/models/talks_model.php
+++ src/system/application/models/talks_model.php
@@ -155,7 +155,7 @@ class Talks_model extends Model {
 		}
 		$res = $q->result();
 
-		if (is_array($res) && is_object($res[0]) && event_isNowOn($res[0]->event_start, $res[0]->event_end)) {
+		if (is_array($res) && isset($res[0]) && is_object($res[0]) && event_isNowOn($res[0]->event_start, $res[0]->event_end)) {
 			$res[0] = talk_decorateNowNext($res[0]);
 		}
 		return $res;
