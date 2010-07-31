d3a5f6482794945d1a65e611ce2e0e3812395c90
Lorna <lorna@lornajane.net>
2010-07-31 22:05:54 +0100

    database patch to add patching history to db
    Currently there's no database awareness so it's easy to get patches out of sync
    This patch adds a history so you know which patches you have run.

--- doc/db/patch8.sql
+++ doc/db/patch8.sql
@@ -1,2 +1,5 @@
-alter table talk_speaker add index idx_talk (talk_id);
+-- patch to create the database versioning tables
 
+CREATE TABLE `patch_history` ( patch_history_id int primary key auto_increment, patch_number int, date_patched timestamp);
+
+INSERT INTO patch_history SET patch_number = 8;
