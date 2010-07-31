d0701bc2d4522973e8d24e74b54b3ec6aa3b527a
Chris Cornutt <enygma@phpdeveloper.org>
2010-07-31 11:24:46 -0500

    correcting bug in speaker name display on event listing

--- src/system/application/views/event/modules/_event_tab_talks.php
+++ src/system/application/views/event/modules/_event_tab_talks.php
@@ -59,11 +59,12 @@
 						foreach($claims[$sp->talk_id] as $c=>$claim){
 							//If it matches exactly or if there's only one claim
 							if($c==$sp->speaker_name || count($claims[$sp->talk_id])==1 && $claim['rcode']!='pending'){
-								$speaker_list[]='<a href="/user/view/'.$claim.'">'.$sp->speaker_name.'</a>';
-							}else{ echo $sp->speaker_name; }
+								$speaker_list[]='<a href="/user/view/'.$claim['uid'].'">'.$sp->speaker_name.'</a>';
+							} //else{ echo $sp->speaker_name; }
 						}
 					}else{ $speaker_list[]=$sp->speaker_name; }
 				}
+				if(empty($speaker_list)){ $speaker_list[]=$sp->speaker_name; }
 				echo implode(', ',$speaker_list);
 				?>
     		</td>
