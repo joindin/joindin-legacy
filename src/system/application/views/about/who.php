853615370a70d751dac3e2c615ee0b8b0fde762c
Chris Cornutt <enygma@phpdeveloper.org>
2010-09-11 15:08:41 -0500

    adding "services" and "who" pages to the about section

---
+++ src/system/application/views/about/who.php
@@ -0,0 +1,14 @@
+<?php
+menu_pagetitle("Who's On Joind.in?");
+
+echo "<h2>Who's on Joind.in?</h2>";
+$rand=array_rand($users,63);
+echo '<table cellpadding="0" cellspacing="0" border=0"><tr>';
+$ct=1;
+foreach($rand as $v){
+	echo '<td style="padding:3px"><a href="/user/view/'.$users[$v].'"><img src="/inc/img/user_gravatar/user'.$users[$v].'.jpg" height="60"></a></td>';
+	if($ct%9==0 && $ct!=0){ echo '</tr><tr>'; }
+	$ct++;
+}
+echo '</tr></table>';
+?>
