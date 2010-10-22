4e2925c6875f62683747ca047a490f5e16e9e801
Mike van Riel <mike.vanriel@naenius.com>
2010-10-22 16:16:58 +0200

    Reformatted another 2 controllers to match the PEAR coding standards

--- src/system/application/controllers/feed.php
+++ src/system/application/controllers/feed.php
@@ -1,134 +1,235 @@
 <?php
 
-class Feed extends Controller {
-	
-	function Feed(){
-		parent::Controller();
-		$this->user_model->logStatus();
-	}
-	function index(){
-		//$this->load->helper('url');
-		redirect();
-	}
-	function talk($tid){
-		$this->load->helper('form');
-		$this->load->model('talks_model');
-		
-		$com=$this->talks_model->getTalkComments($tid,null,null);
-		$tlk=$this->talks_model->getTalks($tid);
-		//echo '<pre>'; print_r($com); echo '</pre>';
-		//echo '<pre>'; print_r($tlk); echo '</pre>';
-		
-		foreach($com as $k=>$v){
-			$items[]=array(
-				'guid'			=> $this->config->site_url() . 'talk/view/'.$v->talk_id,
-				'title'			=> 'Comment on: '.$tlk[0]->talk_title,
-				'link'			=> $this->config->site_url() . 'talk/view/'.$v->talk_id,
-				'description'	=> $v->comment,
-				'pubDate'		=> date('r',$v->date_made)
-			);
-		}
-		//$this->template->write_view('content','feed/feed',$items,TRUE);
-		//$this->template->render();
-		$this->load->view('feed/feed',array('items'=>$items));
-	}
-	function blog(){
-		$this->load->model('blog_posts_model','bpm');
-		$items=array();
-		
-		foreach($this->bpm->getPostDetail() as $k=>$v){
-			//print_r($v);
-			$items[]=array(
-				'guid'			=> $this->config->site_url() . 'blog/view/'.$v->ID,
-				'title'			=> $v->title,
-				'link'			=> $this->config->site_url() . 'blog/view/'.$v->ID,
-				'description'	=> $v->content,
-				'pubDate'		=> date('r',$v->date_posted)
-			);
-		}
-		$this->load->view('feed/feed',array('items'=>$items));
-	}
-	function event($eid){
-		$this->load->model('event_model');
-		$this->load->model('event_comments_model','ecm');
-		
-		$ret    =$this->ecm->getEventComments($eid);
-		$edata	=$this->event_model->getEventDetail($eid);
-		$items=array();
-		foreach($ret as $k=>$v){ //print_r($v);
-			$items[]=array(
-				'guid'			=> $this->config->site_url() . 'event/view/'.$eid.'#comments',
-				'title'			=> 'Comment on Event "'.$edata[0]->event_name.'"',
-				'link'			=> $this->config->site_url() . 'event/view/'.$eid.'#comments',
-				'description'	=> $v->comment,
-				'pubDate'		=> date('r',$v->date_made)
-			);
-		}
-		$this->load->view('feed/feed',array('items'=>$items,'title'=>'Event Comments - "'.$edata[0]->event_name.'"'));
-	}
-	function user($in){
-		$this->load->model('talks_model');
-		$this->load->model('talk_comments_model','tcm');
-		$this->load->model('event_comments_model','ecm');
-		$udata=$this->user_model->getUser($in);
-		$talks		= array();
-		$comments	= array();
-		
-		if(!empty($udata)){
-			$uid=$udata[0]->ID;
-			//get the upcoming talks for this user
-			$ret=$this->talks_model->getUserTalks($uid); //echo '<pre>'; print_r($ret); echo '</pre>';
-			//resort them by date_given
-			$tmp=array(); $out=array();
-			foreach($ret as $k=>$v){ $tmp[$k]=$v->date_given; } arsort($tmp);
-			foreach($tmp as $k=>$v){ $out[]=$ret[$k]; }
-			
-			//$v['title'],$v['desc'],$v['speaker'],$v['date'],$v['tid']);
-			
-			foreach($out as $k=>$v){
-				$talks[]=array(
-					'title'		=> $v->talk_title,
-					'desc'		=> $v->talk_desc,
-					'speaker'	=> $v->speaker,
-					'date'		=> date('r',$v->date_given),
-					'tid'		=> $v->tid,
-					'link'		=> $this->config->site_url() . 'talk/view/'.$v->tid
-				);
-			}
-			$coms=array();
-			
-			//echo '<pre>';
-			//on to the comments!
-			$ecom=$this->ecm->getUserComments($uid);
-			//print_r($ecom);
-			foreach($ecom as $k=>$v){
-				$comments[]=array(
-					'content'		=> $v->comment,
-					'date'			=> date('r',$v->date_made),
-					'type'			=> 'event',
-					'event_id'		=> $v->event_id
-				);
-			}
 
-			$tcom=$this->tcm->getUserComments($uid);
-			//print_r($tcom);
-			foreach($tcom as $k=>$v){
-				$comments[]=array(
-					'content'		=> $v->comment,
-					'date'			=> date('r',$v->date_made),
-					'type'			=> 'talk',
-					'event_id'		=> ''
-				);
-			}
-			//echo '</pre>';
-		}
-		$data=array(
-			'talks'		=> $talks,
-			'comments'	=> $comments,
-			'username'	=> $this->session->userdata('username')
-		);
-		$this->load->view('feed/user',$data);
-	}
+/**
+ * RSS Feed controller.
+ *
+ * PHP version 5
+ *
+ * @category  Joind.in
+ * @package   Controllers
+ * @author    Chris Cornutt <chris@joind.in>
+ * @author    Mike van Riel <mike.vanriel@naenius.com>
+ * @copyright 2009 - 2010 Joind.in
+ * @license   http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ * @link      http://github.com/joindin/joind.in
+ */
+
+/**
+ * RSS Feed controller.
+ *
+ * Responsible for generating and outputting the RSS Feeds.
+ *
+ * @category  Joind.in
+ * @package   Controllers
+ * @author    Chris Cornutt <chris@joind.in>
+ * @author    Mike van Riel <mike.vanriel@naenius.com>
+ * @copyright 2009 - 2010 Joind.in
+ * @license   http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ * @link      http://github.com/joindin/joind.in
+ *
+ * @property  CI_Config   $config
+ * @property  CI_Loader   $load
+ * @property  CI_Template $template
+ * @property  CI_Input    $input
+ * @property  User_model  $user_model
+ */
+class Feed extends Controller
+{
+
+    /**
+     * Constructor, checks whether the user is logged in and passes this to
+     * the template.
+     *
+     * @return void
+     */
+    function Feed()
+    {
+        parent::Controller();
+
+        // check login status and fill the 'logged' parameter in the template
+        $this->user_model->logStatus();
+    }
+
+    /**
+     * The index page redirects the user to the homepage.
+     *
+     * @return void
+     */
+    function index()
+    {
+        redirect();
+    }
+
+    /**
+     * Outputs a feed with the comments of a specific talk.
+     *
+     * @param integer $tid The id of the talk
+     *
+     * @return void
+     */
+    function talk($tid)
+    {
+        $this->load->helper('form');
+        $this->load->model('talks_model');
+
+        $com = $this->talks_model->getTalkComments($tid, null, null);
+        $tlk = $this->talks_model->getTalks($tid);
+
+        foreach ($com as $k => $v) {
+            $guid = $this->config->site_url() . 'talk/view/' . $v->talk_id;
+            $items[] = array(
+                'guid'        => $guid,
+                'title'       => 'Comment on: ' . $tlk[0]->talk_title,
+                'link'        => $guid,
+                'description' => $v->comment,
+                'pubDate'     => date('r', $v->date_made)
+            );
+        }
+
+        $this->load->view(
+            'feed/feed', array(
+                'items' => $items
+            )
+        );
+    }
+
+    /**
+     * Outputs a feed with the posts of the blog.
+     *
+     * @return void
+     */
+    function blog()
+    {
+        $this->load->model('blog_posts_model', 'bpm');
+        $items = array();
+
+        foreach ($this->bpm->getPostDetail() as $k => $v) {
+            $guid = $this->config->site_url() . 'blog/view/' . $v->ID;
+            $items[] = array(
+                'guid'        => $guid,
+                'title'       => $v->title,
+                'link'        => $guid,
+                'description' => $v->content,
+                'pubDate'     => date('r', $v->date_posted)
+            );
+        }
+
+        $this->load->view(
+            'feed/feed', array(
+                'items' => $items
+            )
+        );
+    }
+
+    /**
+     * Outputs a feed with the comments of a specific event.
+     *
+     * @param integer $eid The id of the event
+     *
+     * @return void
+     */
+    function event($eid)
+    {
+        $this->load->model('event_model');
+        $this->load->model('event_comments_model', 'ecm');
+
+        $ret        = $this->ecm->getEventComments($eid);
+        $edata      = $this->event_model->getEventDetail($eid);
+        $event_name = $edata[0]->event_name;
+        $items      = array();
+
+        foreach ($ret as $k => $v) { //print_r($v);
+            $guid = $this->config->site_url() . 'event/view/' . $eid . '#comments';
+            $items[] = array(
+                'guid'        => $guid,
+                'title'       => 'Comment on Event "' . $event_name . '"',
+                'link'        => $guid,
+                'description' => $v->comment,
+                'pubDate'     => date('r', $v->date_made)
+            );
+        }
+
+        $this->load->view(
+            'feed/feed', array(
+                'items' => $items,
+                'title' => 'Event Comments - "' . $event_name . '"'
+            )
+        );
+    }
+
+    /**
+     * Outputs a feed with the event and talk comments of a specific user.
+     *
+     * @param integer $in Id of the user
+     *
+     * @return void
+     */
+    function user($in)
+    {
+        $this->load->model('talks_model');
+        $this->load->model('talk_comments_model', 'tcm');
+        $this->load->model('event_comments_model', 'ecm');
+
+        $udata    = $this->user_model->getUser($in);
+        $talks    = array();
+        $comments = array();
+
+        if (!empty($udata)) {
+            $uid = $udata[0]->ID;
+
+            //get the upcoming talks for this user
+            $ret = $this->talks_model->getUserTalks($uid);
+
+            //re-sort them by date_given
+            $tmp = array();
+            $out = array();
+            foreach ($ret as $k => $v) {
+                $tmp[$k] = $v->date_given;
+            }
+
+            arsort($tmp);
+            foreach ($tmp as $k => $v) {
+                $out[] = $ret[$k];
+            }
+
+            foreach ($out as $k => $v) {
+                $talks[] = array(
+                    'title'   => $v->talk_title, 'desc' => $v->talk_desc,
+                    'speaker' => $v->speaker,
+                    'date'    => date('r', $v->date_given), 'tid' => $v->tid,
+                    'link'    => $this->config->site_url() . 'talk/view/' . $v->tid
+                );
+            }
+
+            //on to the comments!
+            $ecom = $this->ecm->getUserComments($uid);
+            foreach ($ecom as $k => $v) {
+                $comments[] = array(
+                    'content'  => $v->comment,
+                    'date'     => date('r', $v->date_made), 'type' => 'event',
+                    'event_id' => $v->event_id
+                );
+            }
+
+            $tcom = $this->tcm->getUserComments($uid);
+            foreach ($tcom as $k => $v) {
+                $comments[] = array(
+                    'content'  => $v->comment,
+                    'date'     => date('r', $v->date_made), 'type' => 'talk',
+                    'event_id' => ''
+                );
+            }
+        }
+
+        $data = array(
+            'talks'    => $talks,
+            'comments' => $comments,
+            'username' => $this->session->userdata('username')
+        );
+
+        $this->load->view('feed/user', $data);
+    }
 }
 
 ?>
