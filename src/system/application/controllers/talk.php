15de6461e52bfc1ddd95844332ddd14d9e758656
Chris Cornutt <enygma@phpdeveloper.org>
2011-01-02 07:54:51 -0600

    GH-Issue #300 : Track now repopulates when form errors

--- src/system/application/controllers/talk.php
+++ src/system/application/controllers/talk.php
@@ -155,7 +155,8 @@ class Talk extends Controller
             'slides_link'  => 'Slides Link',
             'talk_desc'    => 'Talk Description',
             'session_type' => 'Session Type',
-            'session_lang' => 'Session Language'
+            'session_lang' => 'Session Language',
+			'session_track' => 'Session Track'
         );
         $this->validation->set_rules($rules);
         $this->validation->set_fields($fields);
