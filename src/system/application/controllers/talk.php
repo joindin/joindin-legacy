bd4a527ffef579c1d3f3cb7ac74b6d78361f798d
Chris Cornutt <enygma@phpdeveloper.org>
2011-01-18 12:17:13 -0600

    correcting typo in claims sidebar box

--- src/system/application/controllers/talk.php
+++ src/system/application/controllers/talk.php
@@ -768,7 +768,7 @@ class Talk extends Controller
 		$this->template->write_view('sidebar3','main/_sidebar-block',
 			array(
 				'title'=>'Claiming Talks',
-				'content'=>'<p>Claiming a talk you let us know that you were the speaker for it. When you claim it (and it\'s approved by the event admins) it will be linked to your account.</p><p>You\ll also receive emails when new comments are posted to it.</p>')
+				'content'=>'<p>Claiming a talk you let us know that you were the speaker for it. When you claim it (and it\'s approved by the event admins) it will be linked to your account.</p><p>You\'ll also receive emails when new comments are posted to it.</p>')
 		);
 		
         $this->template->write_view('content', 'talk/detail', $arr, true);
