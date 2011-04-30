5275216cee0942abb76dc8dae247bc6e2ea87bc4
Kevin <kevin@magicmonkey.org>
2011-04-30 20:34:25 +0100

    Added out-of-version-control configs

--- src/scripts/jenkinslaunch.sh
+++ src/scripts/jenkinslaunch.sh
@@ -3,5 +3,13 @@ TARGETBASE=/var/www/joind.in
 TARGET=${TARGETBASE}/${BUILD_NUMBER}
 LAUNCHREF=remotes/magicmonkey/ci-tools
 
-sg web -c "mkdir -p $TARGET ; git archive $LAUNCHREF | tar xC $TARGET && ln -s $TARGET $TARGETBASE/www.new && mv -Tf $TARGETBASE/www.new $TARGETBASE/www"
+sg web -c "
+mkdir -p $TARGET
+ ; git archive $LAUNCHREF | tar xC $TARGET
+&& ln -s $TARGETBASE/config.php $TARGET/src/system/application/config/config.php
+&& ln -s $TARGETBASE/database.php $TARGET/src/system/application/config/database.php
+&& ln -s $TARGET $TARGETBASE/www.new
+&& mv -Tf $TARGETBASE/www.new $TARGETBASE/www
+"
+
 
