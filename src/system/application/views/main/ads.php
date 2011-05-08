a6a6ef5b08d5af978901e33339fb527fa676ceec
Kevin <kevin@magicmonkey.org>
2011-05-08 11:59:04 +0100

    Moved all use of Google ads in to a shared include

---
+++ src/system/application/views/main/ads.php
@@ -0,0 +1,8 @@
+<!-- google ad -->
+    <script type="text/javascript"><!--
+    google_ad_client = "pub-2135094760032194";
+    /* 468x60, created 11/5/08 */
+    google_ad_slot = "4582459016"; google_ad_width = 468; google_ad_height = 60; //-->
+    </script>
+    <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+<!-- end google ad -->
