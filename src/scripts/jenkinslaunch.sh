165eb75177c6c431808a4311b39a94aec463156c
Lorna <lorna@lornajane.net>
2011-05-17 08:21:18 +0100

    add script for database linking on deploy for API

--- src/scripts/jenkinslaunch.sh
+++ src/scripts/jenkinslaunch.sh
@@ -8,9 +8,9 @@ mkdir -p $TARGET \
  ; git archive $LAUNCHREF | tar xC $TARGET \
 && ln -s $TARGETBASE/config.php $TARGET/src/system/application/config/config.php \
 && ln -s $TARGETBASE/database.php $TARGET/src/system/application/config/database.php \
+&& ln -s $TARGETBASE/database.php $TARGET/src/api/database.php \
 && mv $TARGET/src/inc/img/event_icons $TARGET/src/inc/img/event_icons.removed \
 && ln -s $TARGETBASE/tmp/event_icons $TARGET/src/inc/img/event_icons \
-&& ln -s $TARGET $TARGETBASE/www.new \
 && $TARGET/src/scripts/patchdb.sh joindin \
 && mv -Tf $TARGETBASE/www.new $TARGETBASE/www
 "
