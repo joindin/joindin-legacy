3cf4ef5c2ab963995b39b53e3ea1f00fc460bc76
Lorna <lorna@lornajane.net>
2011-05-24 18:44:06 +0100

    adding some clarity to the HTML view to make it useful from a browser

--- src/api-v2/views/HtmlView.php
+++ src/api-v2/views/HtmlView.php
@@ -4,7 +4,29 @@ class HtmlView extends ApiView {
     public function render($content) {
         header('Content-Type: text/html');
         $content = $this->addCount($content);
-        print_r($content);
+        $this->print_array($content);
         return true;
+    }
+
+    protected function print_array($content) {
+        foreach($content as $field => $value) {
+            if(is_array($value)) {
+                // recurse and print a primitive break
+                $this->print_array($value);
+                echo "<br /><br />";
+            } else {
+                // field name
+                echo "<strong>" . $field . ": </strong>";
+                // value, with hyperlinked hyperlinks
+                if(strpos($value, 'http://') === 0) {
+                    echo "<a href=\"" . $value . "\">" . $value . "</a>";
+                } else {
+                    echo $value;
+                }
+                    
+                // newline
+                echo "<br />\n";
+            }
+        }
     }
 }
