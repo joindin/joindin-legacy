2b229ef49484d2b2b56b8c1af550719a13af5671
Brian Fenton <bfenton@ku.edu>
2011-05-27 09:42:33 -0500

    IDE removing extra EOL whitespace

--- src/system/application/libraries/wsactions/talk/Addcomment.php
+++ src/system/application/libraries/wsactions/talk/Addcomment.php
@@ -1,20 +1,20 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
  * Web Service Action: Add a talk comment
  */
 class Addcomment extends BaseWsRequest {
-	
+
 	var $CI	= null;
 	var $xml= null;
-	
+
 	public function Addcomment($xml){
 		$this->CI=&get_instance(); //print_r($this->CI);
 		$this->xml=$xml;
 	}
 	public function checkSecurity($xml){
 		$this->CI->load->model('user_model');
-		
+
 		// Check to see if what they gave us is a valid login
 		// Check for a valid login
 		return ($this->isValidLogin($xml)) ? true : false;
@@ -22,7 +22,7 @@ class Addcomment extends BaseWsRequest {
 	//-----------------------
 	public function run(){
 		$this->CI->load->library('wsvalidate');
-		
+
 		$rules=array(
 			'talk_id'	=>'required',
 			'rating'	=>'required|range[1,5]',
@@ -35,18 +35,18 @@ class Addcomment extends BaseWsRequest {
 		if(!$ret && $unq){
 			$this->CI->load->model('talks_model');
 			$this->CI->load->model('event_model');
-			
+
 			$in			 = (array)$this->xml->action;
 			$talk_detail = $this->CI->talks_model->getTalks($in['talk_id']);
 			$user		 = $this->CI->user_model->getUser($this->xml->auth->user);
-			
+
 			// event ID in $talk_detial[0]->eid
-			
+
 			if(!$talk_detail[0]->allow_comments){
 				// we can't comment on this! same logic as fromtend
 				return $this->throwError('Comments not allowed on the event/talk!');
 			}
-			
+
 			// Ensure this is a valid talk
 			if(empty($talk_detail)){
 				$ret=array('output'=>'json','data'=>array('items'=>array('msg'=>'Invalid talk ID!')));
@@ -75,7 +75,7 @@ class Addcomment extends BaseWsRequest {
 
 			$this->CI->db->insert('talk_comments',$arr);
 			return $this->throwError('Comment added!');
-		}else{ 
+		}else{
 			if(!$unq){ $ret='Non-unique entry!'; }
 			return $this->throwError($ret);
 		}
