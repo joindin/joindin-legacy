21b574b5418b9e068a65f066ab8b60a435d98c39
Lorna <lorna@lornajane.net>
2011-05-30 22:42:03 +0100

    disabling and commenting an existing patch which is breaking deployment.  It isn't needed, and patch history was missing

--- doc/db/patch26.sql
+++ doc/db/patch26.sql
@@ -1,1 +1,5 @@
-alter table events modify event_stub varchar(30) not null, add unique index(event_stub);
+-- patch removed as it causes problems where stubs are emtpy, which is permitted
+-- alter table events modify event_stub varchar(30) not null, add unique index(event_stub);
+
+INSERT INTO patch_history SET patch_number = 26;
+
