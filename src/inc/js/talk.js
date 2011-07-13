756c810aa4d24f12bba10911a4e06e5747540c1c
Rafael Dohms <rdohms@gmail.com>
2011-07-13 00:27:06 -0300

    Added detailed dialog to claim-talk action, to avoid user confusion

--- src/inc/js/talk.js
+++ src/inc/js/talk.js
@@ -27,7 +27,26 @@ talk = function (){
 			}
 			
 			if($('#claim_btn').attr('name')=='single'){
-				return true;
+				
+				$('#claim-dialog').dialog({
+					autoOpen: false,
+					resizable: false,
+					modal: true,
+					buttons: {
+						"Yes, Proceeed": function() {
+							window.location.href = $('#claim_btn').attr('href');
+						},
+						Cancel: function() {
+							$( this ).dialog( "close" );
+						}
+					}
+				});
+				
+				//Open confirmation dialog
+				$( "#claim-dialog" ).dialog('open');
+				
+				//Respond to dialog not link
+				return false;
 			}
 			
 			var obj={ 
