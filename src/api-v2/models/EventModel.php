0feab18c78dede0807eb8dc069551bdb592f6a6a
Lorna <lorna@lornajane.net>
2011-07-30 19:05:37 +0100

    fixing an incorrect website_uri for events

--- src/api-v2/models/EventModel.php
+++ src/api-v2/models/EventModel.php
@@ -278,7 +278,7 @@ class EventModel extends ApiModel
                     . $row['event_id'] . '/comments';
                 $list[$key]['talks_link'] = 'http://' . $host . '/v2/events/' 
                 . $row['event_id'] . '/talks';
-                $list[$key]['website_uri'] = 'http://joind.in/event/view/' . $row['ID'];
+                $list[$key]['website_uri'] = 'http://joind.in/event/view/' . $row['event_id'];
             }
 
             if (count($list) > 1) {
