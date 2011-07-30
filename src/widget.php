23a32165adb7030536809829289f743381b612af
Kevin <kevin@magicmonkey.org>
2011-07-30 19:52:01 +0100

    Removed attempted jQuery loading, and aligned with some data structures from the real API

--- src/widget.php
+++ src/widget.php
@@ -1,30 +1,35 @@
 <?php
 	header("Content-type: text/javascript");
 ?>
-
-
 var joindin = function(){};
-joindin.draw = function(talkId, node) {
-	if (!jQuery) {
-		joindin.writeContent('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" type="text/javascript"></script>', node);
-	}
+joindin.draw = function(talkId, node, postJquery/*=false*/) {
 	if (!node) {
 		document.write('<div id="joindin-content-placeholder"></div>');
 		node = document.getElementById("joindin-content-placeholder");
 	}
-	jQuery.getJSON('//kevin.valinor.local/widgetdata.php?callback=?', {talk:talkId}, function(data){joindin.gotData(data, node);});
+	if (typeof jQuery == 'undefined') {
+		// TODO: Attempt to auto-load jQuery, then relaunch the widget when jQuery is available
+		if (typeof console.log != "undefined") {
+			console.log("No jQuery available - not proceeding with joind.in widget");
+			return;
+		}
+	}
+	// TODO: lookup this URL from the config
+	jQuery.getJSON('//kevin.valinor.local/v2/talks/3214?format=json&callback=?', {talk:talkId}, function(data){joindin.gotData(data, node);});
 }
 
 joindin.gotData = function(data, node) {
+data = data[0];
+data.state = "recent";
 	switch (data.state) {
 		case 'future':
-			joindin.writeContent('<div style="font-size:75%" class="joindin-content-insert joindin-content-insert-future"><a>View this talk on joind.in</a></div>', node);
+			joindin.writeContent('<div style="font-size:75%" class="joindin-content-insert joindin-content-insert-future"><a href="' + data.uri + '">View this talk on joind.in</a></div>', node);
 			break;
 		case 'recent':
-			joindin.writeContent('<div style="font-size:75%" class="joindin-content-insert joindin-content-insert-recent"><img src="//joind.in/inc/img/rating-' + data.average_rating + '.gif" width="75" /> (' + data.comment_count + ')<br /><a>View this talk on joind.in</a></div>', node);
+			joindin.writeContent('<div style="font-size:75%" class="joindin-content-insert joindin-content-insert-recent"><img src="//joind.in/inc/img/rating-' + data.average_rating + '.gif" width="75" /> (' + data.comment_count + ')<br /><a href="' + data.uri + '">Comment on joind.in</a></div>', node);
 			break;
 		case 'past':
-			joindin.writeContent('<div style="font-size:75%" class="joindin-content-insert joindin-content-insert-past"><a>View this talk on joind.in</a></div>', node);
+			joindin.writeContent('<div style="font-size:75%" class="joindin-content-insert joindin-content-insert-past"><img src="//joind.in/inc/img/rating-' + data.average_rating + '.gif" width="75" /> (' + data.comment_count + ')<br /><a href="' + data.uri + '">View on joind.in</a></div>', node);
 			break;
 		default:
 			// Unknown talk
@@ -34,7 +39,6 @@ joindin.gotData = function(data, node) {
 }
 
 joindin.writeContent = function(content, node) {
-console.log(node);
 	jQuery(node).append(content);
 }
 
