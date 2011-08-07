369c80616ebf71dc2970d18782f5d7736902be76
Lorna <lorna@lornajane.net>
2011-08-07 20:44:12 +0100

    updating the API docs to reflect new additions and changed data structures

--- src/system/application/views/api/v2docs.php
+++ src/system/application/views/api/v2docs.php
@@ -21,7 +21,9 @@ This document gives information about the functionality of the API and how to us
 
 <h2>Data Formats</h2>
 
-<p>The service currenty supports <b>JSON</b> and <b>HTML</b> only, although these can very easily be expanded upon in future.  The service will guess from your accept header which format you wanted.  In the event that this is not working correctly then simply add the <b>format</b> parameter to specify which format should be returned.</p>
+<p>The service currenty supports <b>JSON</b> and <b>HTML</b> only, although these can very easily be expanded upon in future.  The service will guess from your accept header which format you wanted.  In the event that this is not behaving as expected, simply add the <b>format</b> parameter to specify which format should be returned.</p>
+
+<p>If you want to use the data provided by this API from JavaScript, we offer support for <b>JSONP</b>.  To use this, request json format data and pass an additional <b>callback</b> parameter; the results will be the usual JSON but surrounded with the function you named.</p>
 
 <p>Where there are links to other resources, and for pagination, you will find those links as part of the response.  The pagination links look something like this:
 <blockquote>
@@ -52,15 +54,21 @@ This document gives information about the functionality of the API and how to us
 
 This is your starting point and will show you where you can go:
 <blockquote>
-<strong>events: </strong><a href="http://api.joind.in/v2/events">http://api.joind.in/v2/events</a><br />
-
-<strong>count: </strong>1<br />
-</blockquote>
-<p>Events lists can also accept the following values for <b>filter</b>:
 <ul>
-    <li><b>hot</b>: current and popular events (<a href="http://api.joind.in/v2/events?filter=hot">try it</a></li>
-    <li><b>upcoming</b>: future events, soonest first (<a href="http://api.joind.in/v2/events?filter=upcoming">try it</a></li>
-    <li><b>past</b>: past events, most recent first (<a href="http://api.joind.in/v2/events?filter=past">try it</a></li>
+
+<li><strong>events:</strong> <a href="http://api.joind.in/v2/events">http://api.joind.in/v2/events</a></li>
+
+<li><strong>hot-events:</strong> <a href="http://api.joind.in/v2/events?filter=hot">http://api.joind.in/v2/events?filter=hot</a></li>
+
+<li><strong>upcoming-events:</strong> <a href="http://api.joind.in/v2/events?filter=upcoming">http://api.joind.in/v2/events?filter=upcoming</a></li>
+
+<li><strong>past-events:</strong> <a href="http://api.joind.in/v2/events?filter=past">http://api.joind.in/v2/events?filter=past</a></li>
+
+<li><strong>open-cfps:</strong> <a href="http://api.joind.in/v2/events?filter=cfp">http://api.joind.in/v2/events?filter=cfp</a></li>
+
+</ul>
+</blockquote>
+
 
 <h3>Request: GET /v2/events</h3>
 <h3>Request: GET /v2/events/[id]</h3>
@@ -73,10 +81,6 @@ Each result looks something like this:</p>
 <blockquote>
 <ul>
 
-<li><strong>0:</strong> <ul>
-
-<li><strong>event_id:</strong> 603</li>
-
 <li><strong>name:</strong> Dutch PHP Conference 2011</li>
 
 <li><strong>start_date:</strong> 2011-05-18T22:00:00+00:00</li>
@@ -87,22 +91,21 @@ Each result looks something like this:</p>
 
 <li><strong>href:</strong> <a href="http://www.phpconference.nl/">http://www.phpconference.nl/</a></li>
 
+<li><strong>attendee_count:</strong> 134</li>
+
 <li><strong>icon:</strong> icon-90x90.png</li>
 
 <li><strong>uri:</strong> <a href="http://api.joind.in/v2/events/603">http://api.joind.in/v2/events/603</a></li>
 
 <li><strong>verbose_uri:</strong> <a href="http://api.joind.in/v2/events/603?verbose=yes">http://api.joind.in/v2/events/603?verbose=yes</a></li>
 
-<li><strong>comments_link:</strong> <a href="http://api.joind.in/v2/events/603/comments">http://api.joind.in/v2/events/603/comments</a></li>
+<li><strong>comments_uri:</strong> <a href="http://api.joind.in/v2/events/603/comments">http://api.joind.in/v2/events/603/comments</a></li>
 
-<li><strong>talks_link:</strong> <a href="http://api.joind.in/v2/events/603/talks">http://api.joind.in/v2/events/603/talks</a></li>
+<li><strong>talks_uri:</strong> <a href="http://api.joind.in/v2/events/603/talks">http://api.joind.in/v2/events/603/talks</a></li>
 
-</ul>
-
-</li>
+<li><strong>website_uri:</strong> <a href="http://joind.in/event/view/603">http://joind.in/event/view/603</a></li>
 
 </ul>
-
 </blockquote>
 
 <h3>Request: GET /events/[id]/talks</h3>
@@ -113,11 +116,7 @@ Each result looks something like this:</p>
 <p>Following the link to the talks for an event gives a list.  The <b>format</b>, <b>start</b> and <b>requestsperpage</b> parameters are valid.  Each talk entry will look something like this:</p>
 
 <blockquote>
-<ul><li><strong>9:</strong> <ul>
-
-<li><strong>talk_id:</strong> 3219</li>
-
-<li><strong>event_id:</strong> 603</li>
+<ul>
 
 <li><strong>talk_title:</strong> ZeroMQ Is The Answer</li>
 
@@ -127,18 +126,62 @@ Each result looks something like this:</p>
 
 <li><strong>start_date:</strong> 2011-05-20T08:45:00+00:00</li>
 
+<li><strong>average_rating:</strong> 4</li>
+
+<li><strong>comments_enabled:</strong> 1</li>
+
+<li><strong>comment_count:</strong> 15</li>
+
+<li><strong>speakers:</strong> <ul>
+
+<li><strong>0:</strong> <ul>
+
 <li><strong>speaker_name:</strong> Ian Barber</li>
+
+<li><strong>speaker_uri:</strong> <a href="http://api.joind.in/v2/users/291">http://api.joind.in/v2/users/291</a></li>
+
+</ul>
+
+</li>
+
+</ul>
+
+</li>
 
 <li><strong>uri:</strong> <a href="http://api.joind.in/v2/talks/3219">http://api.joind.in/v2/talks/3219</a></li>
 
 <li><strong>verbose_uri:</strong> <a href="http://api.joind.in/v2/talks/3219?verbose=yes">http://api.joind.in/v2/talks/3219?verbose=yes</a></li>
 
-<li><strong>comments_link:</strong> <a href="http://api.joind.in/v2/talks/3219/comments">http://api.joind.in/v2/talks/3219/comments</a></li>
+<li><strong>website_uri:</strong> <a href="http://joind.in/talk/view/3219">http://joind.in/talk/view/3219</a></li>
 
-<li><strong>event_link:</strong> <a href="http://api.joind.in/v2/events/603">http://api.joind.in/v2/events/603</a></li>
+<li><strong>comments_uri:</strong> <a href="http://api.joind.in/v2/talks/3219/comments">http://api.joind.in/v2/talks/3219/comments</a></li>
 
-</ul></li></ul>
+<li><strong>event_uri:</strong> <a href="http://api.joind.in/v2/events/603">http://api.joind.in/v2/events/603</a></li>
+
+</ul>
+
 </blockquote>
 
+<h3>Request: GET /talks/[talk_id]/comments</h3>
+<h3>Request: GET /talks/[talk_id]/comments/[comment_id]</h3>
 
+<p>The talk comments (note that event comments are a different thing) include a rating and comment, and if the comment was made by a logged-in user, a link to their user account. The <b>format</b>, <b>start</b> and <b>requestsperpage</b> parameters are valid, and the record for each comment looks something like this:</p>
+
+<blockquote>
+<ul>
+
+<li><strong>rating:</strong> 4</li>
+
+<li><strong>comment:</strong> Interesting discussion and good examples. Will be looking into 0MQ for log aggregation. Also thought it was interesting how many areas of this architecture overlap with some of the principles behind node.js. Well done overall. </li>
+
+<li><strong>uri:</strong> <a href="http://api.joind.in/v2/talks/3219/comments/12578">http://api.joind.in/v2/talks/3219/comments/12578</a></li>
+
+<li><strong>verbose_uri:</strong> <a href="http://api.joind.in/v2/talks/3219/comments/12578?verbose=yes">http://api.joind.in/v2/talks/3219/comments/12578?verbose=yes</a></li>
+
+<li><strong>talk_uri:</strong> <a href="http://api.joind.in/v2/talks/3219">http://api.joind.in/v2/talks/3219</a></li>
+
+<li><strong>user_uri:</strong> <a href="http://api.joind.in/v2/users/14421">http://api.joind.in/v2/users/14421</a></li>
+
+</ul>
+</blockquote> 
 
