abbe2815fff3689e8d68bd6721a5c8ae57933193
Mike van Riel <mike.vanriel@naenius.com>
2012-01-12 22:50:01 +0100

    Altered pull request #456 and added a nice icon, fixed parser error and added some robustness to the code

--- src/system/application/config/twitter.php
+++ src/system/application/config/twitter.php
@@ -1,8 +1,6 @@
 <?php
-die('you haven't set up secrets in src/system/application/config/twitter.php yet);
-$twitter = array(
-
-'consumerKey' => 'replace this with official consumerkey',
-'consumerSecret' => 'replace this with official consumer secret'
-
+die('you haven\'t set up secrets in src/system/application/config/twitter.php yet');
+return array(
+    'consumerKey' => 'replace this with official consumerkey',
+    'consumerSecret' => 'replace this with official consumer secret'
 );
