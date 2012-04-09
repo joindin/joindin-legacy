e65c69a4d5d161ba93116ed955b72c47d212ae93
Lorna Mitchell <lorna@lornajane.net>
2012-04-09 14:35:56 +0100

    altering the way the count element is added to account for the updated data structure

--- src/api-v2/views/ApiView.php
+++ src/api-v2/views/ApiView.php
@@ -1,12 +1,17 @@
 <?php
 
 class ApiView {
-    protected function addCount($data) {
-        if(!empty($data)) {
-            // do nothing, this is added earlier
-        } else {
-            $data['meta']['count'] = 0;
+    protected function addCount($content) {
+        if(is_array($content)) {
+            foreach($content as $name => $item) {
+                // count what's in the non-meta element
+                if($name == "meta") {
+                    continue;
+                } elseif(is_array($item)) {
+                    $content['meta']['count'] = count($item);
+                }
+            }
         }
-        return $data;
+        return $content;
     }
 }
