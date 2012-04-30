884e875cdfe7e570ea4815b726f6719da0ea664d
Lorna Mitchell <lorna@lornajane.net>
2012-04-30 22:34:50 +0100

    adding the user resources to the v2 API

---
+++ src/api-v2/controllers/UsersController.php
@@ -0,0 +1,45 @@
+<?php
+
+class UsersController extends ApiController {
+    public function handle($request, $db) {
+        // only GET is implemented so far
+        if($request->verb == 'GET') {
+            return $this->getAction($request, $db);
+        }
+        return false;
+    }
+
+	public function getAction($request, $db) {
+        $user_id = $this->getItemId($request);
+
+        // verbosity
+        $verbose = $this->getVerbosity($request);
+
+        // pagination settings
+        $start = $this->getStart($request);
+        $resultsperpage = $this->getResultsPerPage($request);
+
+        if(isset($request->url_elements[4])) {
+            switch($request->url_elements[4]) {
+                /*
+                case 'talks':
+                            $talk_mapper = new TalkMapper($db, $request);
+                            $list = $talk_mapper->getTalksByEventId($event_id, $resultsperpage, $start, $request, $verbose);
+                            break;
+                 */
+                default:
+                            throw new InvalidArgumentException('Unknown Subrequest', 404);
+                            break;
+            }
+        } else {
+            $mapper = new UserMapper($db, $request);
+            if($user_id) {
+                $list = $mapper->getUserById($user_id, $verbose);
+            } else {
+                $list = $mapper->getUserList($resultsperpage, $start, $verbose);
+            }
+        }
+
+        return $list;
+	}
+}
