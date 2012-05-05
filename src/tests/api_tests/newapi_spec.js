5ee4c849e7b3a9facb55a8db61025bfe4d9e0313
Lorna Mitchell <lorna@lornajane.net>
2012-05-05 21:38:00 +0100

    moving things around in the testing file so it is easier to follow the flow and re-use per-resource tests

--- src/tests/api_tests/newapi_spec.js
+++ src/tests/api_tests/newapi_spec.js
@@ -1,3 +1,4 @@
+// vim: tabstop=2:softtabstop=2:shiftwidth=2 
 // ./node_modules/.bin/jasmine-node . 
 var frisby = require('frisby');
 var util   = require('util');
@@ -6,13 +7,13 @@ var baseURL;
 if (typeof process.env.API_BASE_URL != 'undefined') {
 	baseURL = process.env.API_BASE_URL;
 } else {
-	baseURL = "http://api.joind.in";
+	baseURL = "http://api.dev.joind.in:8080";
 }
 
 frisby.globalSetup({ // globalSetup is for ALL requests
-    request: {
-      headers: { 'Content-type': 'application/json' }
-    }
+  request: {
+    headers: { 'Content-type': 'application/json' }
+  }
 });
 
 frisby.create('Initial discovery')
@@ -42,137 +43,31 @@ frisby.create('Initial discovery')
           expect(ev).toContainJsonTypes({"events":Array});
 
           for(var i in ev.events) {
-              if (ev.events[i].href != null) {
-                expect(ev.events[i].href).toBeDefined();
-                expect(typeof ev.events[i].href).toBe('string');
-                if (ev.events[i].href != '') {
-                  //expect(ev.events[i].href).toMatch(/^http/);
-  			    }
-              }
-              if (ev.events[i].icon != null) {
-                expect(ev.events[i].icon).toBeDefined();
-                expect(typeof ev.events[i].icon).toBe('string');
-              }
-         
-             // Check required fields
-              expect(ev.events[i].name).toBeDefined();
-              expect(ev.events[i].start_date).toBeDefined();
-              expect(ev.events[i].end_date).toBeDefined();
-              expect(ev.events[i].description).toBeDefined();
-              expect(ev.events[i].href).toBeDefined();
-              expect(ev.events[i].icon).toBeDefined();
-              expect(ev.events[i].attendee_count).toBeDefined();
-              expect(ev.events[i].uri).toBeDefined();
-              expect(ev.events[i].verbose_uri).toBeDefined();
-              expect(ev.events[i].comments_uri).toBeDefined();
-              expect(ev.events[i].talks_uri).toBeDefined();
-              expect(ev.events[i].website_uri).toBeDefined();
-              expect(typeof ev.events[i].name).toBe('string');
-              checkDate(ev.events[i].start_date);
-              checkDate(ev.events[i].end_date);
-              expect(typeof ev.events[i].description).toBe('string');
-              expect(typeof ev.events[i].attendee_count).toBe('number');
-              expect(typeof ev.events[i].uri).toBe('string');
-              expect(typeof ev.events[i].verbose_uri).toBe('string');
-              expect(typeof ev.events[i].comments_uri).toBe('string');
-              expect(typeof ev.events[i].talks_uri).toBe('string');
-              expect(typeof ev.events[i].website_uri).toBe('string');
+            checkEvent(ev.events[i]);
   		
           // Check for more detail in the events
-              frisby.create('Event detail for ' + ev.events[i].name)
-                .get(ev.events[i].verbose_uri)
-                .expectStatus(200)
-                .expectHeader("content-type", "application/json; charset=utf8")
-                .afterJSON(function(detailedEv) {
-                  expect(detailedEv.events[0]).toBeDefined();
-                  expect(typeof detailedEv.events[0]).toBe('object');
-                  var evt = detailedEv.events[0];
-                  expect(evt.name).toBeDefined();
-                  expect(evt.start_date).toBeDefined();
-                  expect(evt.end_date).toBeDefined();
-                  expect(evt.description).toBeDefined();
-                  expect(evt.href).toBeDefined();
-                  expect(evt.icon).toBeDefined();
-                  expect(evt.latitude).toBeDefined();
-                  expect(evt.longitude).toBeDefined();
-                  expect(evt.tz_continent).toBeDefined();
-                  expect(evt.tz_place).toBeDefined();
-                  expect(evt.location).toBeDefined();
-                  expect(evt.attendee_count).toBeDefined();
-                  expect(evt.comments_enabled).toBeDefined();
-                  expect(evt.event_comments_count).toBeDefined();
-                  expect(evt.cfp_start_date).toBeDefined();
-                  expect(evt.cfp_end_date).toBeDefined();
-                  expect(evt.cfp_url).toBeDefined();
-                  expect(evt.uri).toBeDefined();
-                  expect(evt.verbose_uri).toBeDefined();
-                  expect(evt.comments_uri).toBeDefined();
-                  expect(evt.talks_uri).toBeDefined();
-                  expect(evt.website_uri).toBeDefined();
-                  expect(evt.all_talk_comments_uri).toBeDefined();
-
-                  expect(typeof evt.name).toBe('string', "Event name");
-                  checkDate(evt.start_date);
-                  checkDate(evt.end_date);
-                  expect(typeof evt.description).toBe('string');
-                  if (evt.href != null) {
-                    expect(typeof evt.href).toBe('string', "Event href");
-                  }
-                  if (evt.icon != null) {
-                    expect(typeof evt.icon).toBe('string');
-                  }
-                  expect(typeof evt.latitude).toBe('number');
-                  expect(typeof evt.longitude).toBe('number');
-                  expect(typeof evt.tz_continent).toBe('string');
-                  expect(typeof evt.tz_place).toBe('string');
-                  expect(typeof evt.location).toBe('string');
-                  expect(typeof evt.attendee_count).toBe('number');
-                  expect(typeof evt.comments_enabled).toBe('number');
-                  expect(typeof evt.event_comments_count).toBe('number');
-                  if (evt.cfp_start_date != null) {
-                      checkDate(evt.cfp_start_date);
-                  }
-                  if (evt.cfp_end_date != null) {
-                      checkDate(evt.cfp_end_date);
-                  }
-                  if (evt.cfp_url != null) {
-                    expect(typeof evt.cfp_url).toBe('string');
-                  }
-                  expect(typeof evt.uri).toBe('string');
-                  expect(typeof evt.verbose_uri).toBe('string');
-                  expect(typeof evt.comments_uri).toBe('string');
-                  expect(typeof evt.talks_uri).toBe('string');
-                  expect(typeof evt.website_uri).toBe('string');
-                  expect(typeof evt.all_talk_comments_uri).toBe('string');
+            frisby.create('Event detail for ' + ev.events[i].name)
+              .get(ev.events[i].verbose_uri)
+              .expectStatus(200)
+              .expectHeader("content-type", "application/json; charset=utf8")
+              .afterJSON(function(detailedEv) {
+                expect(detailedEv.events[0]).toBeDefined();
+                expect(typeof detailedEv.events[0]).toBe('object');
+                var evt = detailedEv.events[0];
+                checkVerboseEvent(evt);
 
 
-                  frisby.create('Event comments for ' + evt.name)
-                    .get(evt.comments_uri)
-                    .expectStatus(200)
-                    .expectHeader("content-type", "application/json; charset=utf8")
-                    .afterJSON(function(evComments) {
-                        if(typeof evComments.comments == 'object') {
-                            for(var i in evComments.comments) {
-                                var comment = evComments.comments[i];
-
-                                expect(comment.comment).toBeDefined();
-                                expect(typeof comment.comment).toBe('string');
-                                expect(comment.created_date).toBeDefined();
-                                checkDate(comment.created_date);
-                                expect(comment.user_display_name).toBeDefined();
-                                if(typeof comment.user_uri != 'undefined') {
-                                    expect(typeof comment.user_uri).toBe('string');
-                                }
-                                expect(comment.comment_uri).toBeDefined();
-                                expect(typeof comment.comment_uri).toBe('string');
-                                expect(comment.verbose_comment_uri).toBeDefined();
-                                expect(typeof comment.verbose_comment_uri).toBe('string');
-                                expect(comment.event_uri).toBeDefined();
-                                expect(typeof comment.event_uri).toBe('string');
-                                expect(comment.event_comments_uri).toBeDefined();
-                                expect(typeof comment.event_comments_uri).toBe('string');
-                            }
-                        }
+                frisby.create('Event comments for ' + evt.name)
+                  .get(evt.comments_uri)
+                  .expectStatus(200)
+                  .expectHeader("content-type", "application/json; charset=utf8")
+                  .afterJSON(function(evComments) {
+                    if(typeof evComments.comments == 'object') {
+                      for(var i in evComments.comments) {
+                        var comment = evComments.comments[i];
+                        checkEventComment(comment);
+                      }
+                    }
 					})
 				  .toss();
 
@@ -185,9 +80,9 @@ frisby.create('Initial discovery')
 .toss();
 
 function checkDate(fieldValue) {
-    dateVal = new Date(fieldValue);
-    expect(getObjectClass(dateVal)).toBe('Date');
-    return true;
+  dateVal = new Date(fieldValue);
+  expect(getObjectClass(dateVal)).toBe('Date');
+  return true;
 }
 
 /**
@@ -196,15 +91,131 @@ function checkDate(fieldValue) {
  * stolen from: http://blog.magnetiq.com/post/514962277/finding-out-class-names-of-javascript-objects
  */
 function getObjectClass(obj) {
-    if (obj && obj.constructor && obj.constructor.toString) {
-        var arr = obj.constructor.toString().match(
-            /function\s*(\w+)/);
+  if (obj && obj.constructor && obj.constructor.toString) {
+    var arr = obj.constructor.toString().match(
+      /function\s*(\w+)/);
 
-        if (arr && arr.length == 2) {
-            return arr[1];
-        }
+    if (arr && arr.length == 2) {
+      return arr[1];
     }
+  }
 
-    return undefined;
+  return undefined;
 }
 
+function checkEvent(ev) {
+  if (ev.href != null) {
+    expect(ev.href).toBeDefined();
+    expect(typeof ev.href).toBe('string');
+    if (ev.href != '') {
+      //expect(ev.href).toMatch(/^http/);
+    }
+  }
+  if (ev.icon != null) {
+    expect(ev.icon).toBeDefined();
+    expect(typeof ev.icon).toBe('string');
+  }
+
+ // Check required fields
+  expect(ev.name).toBeDefined();
+  expect(ev.start_date).toBeDefined();
+  expect(ev.end_date).toBeDefined();
+  expect(ev.description).toBeDefined();
+  expect(ev.href).toBeDefined();
+  expect(ev.icon).toBeDefined();
+  expect(ev.attendee_count).toBeDefined();
+  expect(ev.uri).toBeDefined();
+  expect(ev.verbose_uri).toBeDefined();
+  expect(ev.comments_uri).toBeDefined();
+  expect(ev.talks_uri).toBeDefined();
+  expect(ev.website_uri).toBeDefined();
+  expect(typeof ev.name).toBe('string');
+  checkDate(ev.start_date);
+  checkDate(ev.end_date);
+  expect(typeof ev.description).toBe('string');
+  expect(typeof ev.attendee_count).toBe('number');
+  expect(typeof ev.uri).toBe('string');
+  expect(typeof ev.verbose_uri).toBe('string');
+  expect(typeof ev.comments_uri).toBe('string');
+  expect(typeof ev.talks_uri).toBe('string');
+  expect(typeof ev.website_uri).toBe('string');
+}
+
+function checkVerboseEvent(evt) {
+  expect(evt.name).toBeDefined();
+  expect(evt.start_date).toBeDefined();
+  expect(evt.end_date).toBeDefined();
+  expect(evt.description).toBeDefined();
+  expect(evt.href).toBeDefined();
+  expect(evt.icon).toBeDefined();
+  expect(evt.latitude).toBeDefined();
+  expect(evt.longitude).toBeDefined();
+  expect(evt.tz_continent).toBeDefined();
+  expect(evt.tz_place).toBeDefined();
+  expect(evt.location).toBeDefined();
+  expect(evt.attendee_count).toBeDefined();
+  expect(evt.comments_enabled).toBeDefined();
+  expect(evt.event_comments_count).toBeDefined();
+  expect(evt.cfp_start_date).toBeDefined();
+  expect(evt.cfp_end_date).toBeDefined();
+  expect(evt.cfp_url).toBeDefined();
+  expect(evt.uri).toBeDefined();
+  expect(evt.verbose_uri).toBeDefined();
+  expect(evt.comments_uri).toBeDefined();
+  expect(evt.talks_uri).toBeDefined();
+  expect(evt.website_uri).toBeDefined();
+  expect(evt.all_talk_comments_uri).toBeDefined();
+
+  expect(typeof evt.name).toBe('string', "Event name");
+  checkDate(evt.start_date);
+  checkDate(evt.end_date);
+  expect(typeof evt.description).toBe('string');
+  if (evt.href != null) {
+    expect(typeof evt.href).toBe('string', "Event href");
+  }
+  if (evt.icon != null) {
+    expect(typeof evt.icon).toBe('string');
+  }
+  expect(typeof evt.latitude).toBe('number');
+  expect(typeof evt.longitude).toBe('number');
+  expect(typeof evt.tz_continent).toBe('string');
+  expect(typeof evt.tz_place).toBe('string');
+  expect(typeof evt.location).toBe('string');
+  expect(typeof evt.attendee_count).toBe('number');
+  expect(typeof evt.comments_enabled).toBe('number');
+  expect(typeof evt.event_comments_count).toBe('number');
+  if (evt.cfp_start_date != null) {
+    checkDate(evt.cfp_start_date);
+  }
+  if (evt.cfp_end_date != null) {
+    checkDate(evt.cfp_end_date);
+  }
+  if (evt.cfp_url != null) {
+    expect(typeof evt.cfp_url).toBe('string');
+  }
+  expect(typeof evt.uri).toBe('string');
+  expect(typeof evt.verbose_uri).toBe('string');
+  expect(typeof evt.comments_uri).toBe('string');
+  expect(typeof evt.talks_uri).toBe('string');
+  expect(typeof evt.website_uri).toBe('string');
+  expect(typeof evt.all_talk_comments_uri).toBe('string');
+}
+
+function checkEventComment(comment) {
+  expect(comment.comment).toBeDefined();
+  expect(typeof comment.comment).toBe('string');
+  expect(comment.created_date).toBeDefined();
+  checkDate(comment.created_date);
+  expect(comment.user_display_name).toBeDefined();
+  if(typeof comment.user_uri != 'undefined') {
+      expect(typeof comment.user_uri).toBe('string');
+  }
+  expect(comment.comment_uri).toBeDefined();
+  expect(typeof comment.comment_uri).toBe('string');
+  expect(comment.verbose_comment_uri).toBeDefined();
+  expect(typeof comment.verbose_comment_uri).toBe('string');
+  expect(comment.event_uri).toBeDefined();
+  expect(typeof comment.event_uri).toBe('string');
+  expect(comment.event_comments_uri).toBeDefined();
+  expect(typeof comment.event_comments_uri).toBe('string');
+}
