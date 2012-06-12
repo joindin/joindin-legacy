7d8d6f3446a0450734b65098d38e63a413a7e425
Rob Allen <rob@akrabat.com>
2012-06-12 14:58:50 +0100

    Remove undefined  variable

--- src/system/application/libraries/Timezone.php
+++ src/system/application/libraries/Timezone.php
@@ -95,7 +95,7 @@ class Timezone {
         $dateObj->setDate($year, $month, $day);
         $dateObj->setTime($hour, $minute, $second);
 
-        $unixTimestamp = $dateObj->format("U") - $unix_correction;
+        $unixTimestamp = $dateObj->format("U");
 
 /* Commented out as it appears to be causing a DST bug - see JOINDIN-169 in Jira
         // How much wrong will ->format("U") be if I do it now, due to DST changes?
