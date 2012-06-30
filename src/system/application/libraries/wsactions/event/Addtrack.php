313ebb8e02de08f7e8c44de4aa1be19540d3d049
Lorna Mitchell <lorna@lornajane.net>
2012-06-30 16:02:41 +0100

    removing nonsense int checks from the wsactions classes on users

--- src/system/application/libraries/wsactions/event/Addtrack.php
+++ src/system/application/libraries/wsactions/event/Addtrack.php
@@ -17,17 +17,20 @@ class Addtrack extends BaseWsRequest {
         if ($this->CI->um->isAuth() || $this->isValidLogin($xml)) {
             // They either need to be an admin of the event or a site admin
             
+            $user= false;
             if ($this->CI->um->isAuth()) {
                 $user=$this->CI->session->userdata('username');
             } elseif (!$this->CI->um->isAuth()) {
                 $user=$xml->auth->user;
             }
-            if (!is_int($user)) { 
-                $udata=$this->CI->um->getUserByUsername($user);
-                if (!empty($udata)) { 
-                    $user=$udata[0]->ID;
-                } else { return false; }
+
+            $udata=$this->CI->um->getUserByUsername($user);
+            if (!empty($udata)) { 
+                $user=$udata[0]->ID;
+            } else { 
+                return false; 
             }
+
             $eid	= (int)$xml->action->event_id;
             $rtype	= 'event';
             
