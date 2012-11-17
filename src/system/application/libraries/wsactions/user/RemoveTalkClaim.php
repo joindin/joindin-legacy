6286dc60bc201bc3067feaee5ad8278de2cab9b7
David Stockton <github@davidstockton.com>
2012-11-17 11:36:24 -0700

    Codesniff cleanup for webservice to remove a user's claim on a talk

--- src/system/application/libraries/wsactions/user/RemoveTalkClaim.php
+++ src/system/application/libraries/wsactions/user/RemoveTalkClaim.php
@@ -1,51 +1,91 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+/**
+ * Joindin webservice for removing a claim on a talk
+ *
+ * PHP version 5
+ *
+ * @category  Joind.in
+ * @package   Configuration
+ * @copyright 2009 - 2012 Joind.in
+ * @license   http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ */
 
-// Remove a user's claim on a talk
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
-class RemoveTalkClaim extends BaseWsRequest {
+/**
+ * Joindin webservice for removing a claim on a talk
+ *
+ * PHP version 5
+ *
+ * @category  Joind.in
+ * @package   Configuration
+ * @copyright 2009 - 2012 Joind.in
+ * @license   http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ */
+class RemoveTalkClaim extends BaseWsRequest
+{
 
-    var $CI		= null;
-    var $xml	= null;
+    public $CI  = null;
+    public $xml = null;
 
-    public function RemoveTalkClaim($xml) {
-        $this->CI=&get_instance(); //print_r($this->CI);
-        $this->xml=$xml;
+    /**
+     * Builds the webservice for removing a claim on a talk
+     *
+     * @param string $xml XML to set
+     */
+    public function __construct($xml) 
+    {
+        $this->CI  = &get_instance(); //print_r($this->CI);
+        $this->xml = $xml;
     }
 
     /**
-    * Only site admins can use this functionality
-    */
-    public function checkSecurity($xml) {
-
+     * Ensures that only site admins can use this functionality
+     *
+     * @param string $xml XML to check
+     *
+     * @return boolean
+     */
+    public function checkSecurity($xml) 
+    {
         $this->CI->load->model('user_model');
 
         // Check for a valid login
         //if ($this->isValidLogin($xml) || $this->CI->user_model->isAuth()) {
         if ($this->CI->user_model->isAuth()) {
             // Now check to see if they're a site admin
-            $user=$this->CI->session->userdata('username');
+            $user = $this->CI->session->userdata('username');
             if (!$this->CI->user_model->isSiteAdmin($user)) {
                 return false;
-            } else { return true; }
-
-        } else { return false; }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 
-    //-----------------------
-    public function run() {
-
+    /**
+     * Runs the webservices and removes a claim on a talk
+     *
+     * @return array
+     */
+    public function run() 
+    {
         $this->CI->load->model('pending_talk_claims_model');
 
-        $result = array();
-        $claim_id = (int)$this->xml->action->claim_id;
+        $result     = array();
+        $claim_id   = (int)$this->xml->action->claim_id;
         $css_row_id = (string)$this->xml->action->css_row_id;
-        $result['claim_id'] = $claim_id;
+
+        $result['claim_id']   = $claim_id;
         $result['css_row_id'] = $css_row_id;
 
         $this->CI->pending_talk_claims_model->deleteClaim($claim_id);
 
         $result['msg'] = 'Success';
-        return array('output'=>'json','items'=>$result);
+        return array('output'=>'json', 'items'=>$result);
     }
-
 }
