fe8f08820def7d1d3c822bc112a8a3232099d18d
David Stockton <github@davidstockton.com>
2012-12-20 23:31:34 -0700

    Codesniff cleanup for spam class

--- src/system/application/libraries/Spam.php
+++ src/system/application/libraries/Spam.php
@@ -1,41 +1,116 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+<?php
+/**
+ * Spam Class
+ *
+ * PHP version 5
+ *
+ * @category  Joind.in
+ * @package   Configuration
+ * @copyright 2009 - 2012 Joind.in
+ * @license   http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ */
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+/**
+ * Spam Class
+ *
+ * PHP version 5
+ *
+ * @category  Joind.in
+ * @package   Configuration
+ * @copyright 2009 - 2012 Joind.in
+ * @license   http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ *
+ * @todo      Determine if this is even used at all
+ */
+class Spam
+{
 
-class Spam {
-    
-    var $source	= 'db';
-    var $CI		= null;
-    
-    // Chk values: regex, substr
-    function check($chk, $val, $source='db') { 
-        $this->CI =& get_instance();
-        $pass=true;
+    public $source = 'db';
+    public $CI     = null;
+
+    /**
+     * Does something. Not sure what.
+     *
+     * @param mixed  $chk    A parameter
+     * @param mixed  $val    Another parameter
+     * @param string $source Something else
+     *
+     * @return bool
+     */
+    public function check($chk, $val, $source = 'db')
+    {
+        $this->CI = & get_instance();
+        $pass     = true;
         if (is_array($chk)) {
-            foreach ($chk as $k=>$v) {
-                $ret=$this->{'chk_'.$v}($val);
+            foreach ($chk as $k => $v) {
+                $ret = $this->{'chk_' . $v}($val);
             }
-        } else { $pass=$this->{'chk_'.$chk}($val); }
+        } else {
+            $pass = $this->{'chk_' . $chk}($val);
+        }
+
         return $pass;
     }
-    function fetch_db($type) {
-        
+
+    /**
+     * Does nothing. Empty
+     *
+     * @param null $type Not used
+     *
+     * @return void
+     */
+    public function fetch_db($type)
+    {
+
     }
-    function fetch_txt($type='regex') {
-        $txt_list=$this->CI->config->item('blacklist_'.$type);
+
+    /**
+     * Does something and then reads from a file
+     *
+     * @param string $type Type of thing for doing something
+     *
+     * @return array
+     */
+    public function fetch_txt($type = 'regex')
+    {
+        $txt_list = $this->CI->config->item('blacklist_' . $type);
+
         return file($txt_list);
     }
-    //--------------------
-    // Check functions
-    function chk_substr($val) {
-        
+
+    /**
+     * Does nothing
+     *
+     * @param null $val Does nothing
+     *
+     * @return void
+     */
+    public function chk_substr($val)
+    {
+
     }
-    function chk_regex($val) { 
-        $pass=true;
-        $rows=$this->fetch_txt('regex'); 
-        foreach ($rows as $k=>$v) {
-            $m=preg_match($v, $val);
-            if ($m) { return false; }
+
+    /**
+     * Checks a value of something with a regex from a place
+     *
+     * @param string $val A value to check for something
+     *
+     * @return bool
+     */
+    public function chk_regex($val)
+    {
+        $pass = true;
+        $rows = $this->fetch_txt('regex');
+        foreach ($rows as $k => $v) {
+            $m = preg_match($v, $val);
+            if ($m) {
+                return false;
+            }
         }
+
         return true;
     }
-    
+
 }
