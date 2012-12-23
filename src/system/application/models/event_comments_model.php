17c7fb35ed24c0a298c15d8e36d11a18711b59f9
David Stockton <github@davidstockton.com>
2012-12-23 11:50:34 -0700

    Codesniff cleanup for event comments model

--- src/system/application/models/event_comments_model.php
+++ src/system/application/models/event_comments_model.php
@@ -1,34 +1,98 @@
 <?php
+/**
+ * Event comments model
+ *
+ * PHP version 5
+ *
+ * @category  Joind.in
+ * @package   Configuration
+ * @copyright 2009 - 2012 Joind.in
+ * @license   http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ */
 
-class Event_comments_model extends Model {
+/**
+ * Event comments model
+ *
+ * PHP version 5
+ *
+ * @category  Joind.in
+ * @package   Configuration
+ * @copyright 2009 - 2012 Joind.in
+ * @license   http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ */
+class Event_comments_model extends Model
+{
+    /**
+     * Determines if a comment is unique
+     *
+     * @param array $data Comment data
+     *
+     * @return bool
+     */
+    public function isUnique($data)
+    {
+        $q   = $this->db->get_where('event_comments', $data);
+        $ret = $q->result();
 
-    function Event_comments_model() {
-        parent::Model();
-    }
-    //------------------
-    function isUnique($data) {
-        $q=$this->db->get_where('event_comments', $data);
-        $ret=$q->result();
         return (empty($ret)) ? true : false;
     }
-    function getEventComments($eid) {
+
+    /**
+     * Retrieves the comments for an event
+     *
+     * @param integer $eid Event id
+     *
+     * @return mixed
+     */
+    public function getEventComments($eid)
+    {
         $this->db->from('event_comments');
         $this->db->where('event_id', $eid);
-        $q=$this->db->get();
+        $q = $this->db->get();
+
         return $q->result();
     }
-    function getUserComments($uid) {
+
+    /**
+     * Retrieves a user's comments
+     *
+     * @param integer $uid User id
+     *
+     * @return mixed
+     */
+    public function getUserComments($uid)
+    {
         $this->db->from('event_comments');
         $this->db->where('user_id', $uid);
-        $q=$this->db->get();
+        $q = $this->db->get();
+
         return $q->result();
     }
-    function deleteComment($cid) {
-        $this->db->delete('event_comments', array('id'=>$cid));
+
+    /**
+     * Deletes a comment
+     *
+     * @param integer $cid Comment id
+     *
+     * @return void
+     */
+    public function deleteComment($cid)
+    {
+        $this->db->delete('event_comments', array('id' => $cid));
     }
-    function getCommentDetail($cid) {
-        $q=$this->db->get_where('event_comments', array('ID'=>$cid));
-        return $q->result();	
+
+    /**
+     * Retrieves comment details
+     *
+     * @param integer $cid Comment id
+     *
+     * @return mixed
+     */
+    public function getCommentDetail($cid)
+    {
+        $q = $this->db->get_where('event_comments', array('ID' => $cid));
+
+        return $q->result();
     }
 }
 
