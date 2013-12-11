0feed76a2f6c81bdc001d2c9e5910b6ceac66847
David Stockton <github@davidstockton.com>
2013-12-10 23:40:13 -0700

    Code cleanup for MY_Loader.php

--- src/system/application/libraries/MY_Loader.php
+++ src/system/application/libraries/MY_Loader.php
@@ -1,21 +1,48 @@
 <?php
+/**
+ * Joind.in loader
+ *
+ * @category Joind.in
+ * @package  Libraries
+ * @license  http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ * @link     http://github.com/joindin/joind.in
+ */
 
-class MY_Loader extends CI_Loader 
+/**
+ * Joind.in loader
+ *
+ * @category Joind.in
+ * @package  Libraries
+ * @license  http://github.com/joindin/joind.in/blob/master/doc/LICENSE JoindIn
+ * @link     http://github.com/joindin/joind.in
+ */
+class MY_Loader extends CI_Loader
 {
-    
-    private function getModelPath()
+    /**
+     * Returns the path to the models
+     *
+     * @return string
+     */
+    private function _getModelPath()
     {
         return APPPATH.'models';
     }
-    
+
+    /**
+     * Loads the model object requested
+     *
+     * @param string $objectType Object type to load
+     * @param null   $id         Not used
+     *
+     * @return mixed
+     */
     public function model_obj($objectType, $id = null)
     {
         // See if there's a model that exists
         $modelName = ucwords(strtolower($objectType));
-        $modelFile = $this->getModelPath().'/'.$modelName.'.php';
-        if (is_file($modelFile))
-        {
-            include_once($modelFile);
+        $modelFile = $this->_getModelPath() . '/' . $modelName . '.php';
+        if (is_file($modelFile)) {
+            include_once $modelFile;
             return new $modelName($id);
         } else {
             return null;
