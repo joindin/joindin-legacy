2cdb01b654f9fd7c89db973e3dd9d66b10737efa
Rob Allen <rob@akrabat.com>
2014-01-25 22:57:13 +0000

    We really don't need to advertise API v1.

--- src/system/application/views/api/doc.php
+++ src/system/application/views/api/doc.php
@@ -12,4 +12,4 @@ b.req_name { font-size: 12px; }
 <p>Joind.in's API can be found at <a href="http://api.joind.in">http://api.joind.in</a> and its documentation is here: <A href="http://joindin.github.io/joindin-api/">http://joindin.github.io/joindin-api</a>.  You can find the code on github <a href="https://github.com/joindin/joindin-api">https://github.com/joindin/joindin-api</a></p>
 
 
-There is/was a legacy v1 API, which is no longer supported.  However there are some <a href="/api/v1docs">API v1 docs</a> still available</p>
+<p>The legacy v1 API is no longer supported.</p>
