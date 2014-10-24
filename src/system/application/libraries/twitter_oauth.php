79b40b4ad707f1948d3f1d2d0ad17c74ded4e4ff
Scott Wilcox <scott@dor.ky>
2014-10-24 13:18:51 +0100

    Use CURL_SSLVERSION_TLSv1 for outbound ssl connections
    This corrects the issues with Twitter disabling SSLv3 in their API due to POODLE.

--- src/system/application/libraries/twitter_oauth.php
+++ src/system/application/libraries/twitter_oauth.php
@@ -192,7 +192,7 @@ class twitter_oauth
     {
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+        curl_setopt($ch, CURLOPT_SSLVERSION, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
